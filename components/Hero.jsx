2a14a864deda381bc3f8d3d0b542f9b1363daf04
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2023-08-08 10:36:23 +0530

    fixed mobile issues

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
     return (
         <section className="relative flex flex-col justify-center ml-[6.5rem] w-screen mt-[4.25rem] h-screen mx-auto">
             <div
-                className={`${styles.paddingX} max-w-7xl mx-auto flex pb-5 pt-[4rem] mt-5 ml-10 flex-row items-start gap-5 `}
+                className={` max-w-7xl mx-auto flex pb-5 pt-[4rem] mt-5 ml-1 flex-row items-start gap-5 `}
             >
-                <div className="flex flex-col justify-center items-center mt-5">
+                <div className="flex flex-col justify-center items-center mt-5 ml-[-4.80rem]">
                     <div className="w-5 h-5 rounded-full bg-[#915eff]  " />
                     <div className="w-1 sm:h-80 h-40 violet-gradient" />
                 </div>
@@ -20,9 +20,10 @@ const Hero = () => {
                         <span className="text-[#915eff]  "> Prakash</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                        I&apos;m a full stack developer,
-                        <br className="sm:block hidden" />
-                        freelancer and love to learn new skills
+                        I&apos;m a full stack web developer, 
+                        <br />
+                        freelancer and <br className="sm:hidden block" />
+                        I love to learn new skills
                     </p>
                 </div>
             </div>
